812ab627e647f8627c9ca4072d08511a141ca8dc
Túbal Martín <tubal.martin@hola-internet.com>
2013-11-14 10:10:05 +0100

    Set dependencies as peerDependencies Improved index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var path = require('path');
 
-var pattern = function(file) {
-  return {pattern: file, included: true, served: true, watched: false};
+var createPattern = function(path) {
+  return {pattern: path, included: true, served: true, watched: false};
 };
 
 var framework = function(files) {
-  files.unshift(pattern(path.join(__dirname, 'adapter.js')));
-  files.unshift(pattern(path.resolve(require.resolve('chai-sinon'))));
-  files.unshift(pattern(path.resolve(require.resolve('chai'), '../chai.js')));
-  files.unshift(pattern(path.resolve(require.resolve('sinon'), '../../pkg/sinon.js')));
+  files.unshift(createPattern(__dirname + '/adapter.js'));
+  files.unshift(createPattern(path.dirname(require.resolve('sinon-chai')) + '/sinon-chai.js'));
+  files.unshift(createPattern(path.dirname(require.resolve('chai')) + '/chai.js'));
+  files.unshift(createPattern(path.dirname(require.resolve('sinon')) + '/../pkg/sinon.js'));
 };
 
 framework.$inject = ['config.files'];
